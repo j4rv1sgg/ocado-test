92fb212cb45d5dd0540a8773ef7c3fde1a2ff8c1
j4rv1sgg <jrvs.inc@gmail.com>
2025-05-09 14:41:53 +0200

    adjust project structure & add typed data

---
+++ src/types/index.ts
@@ -0,0 +1,15 @@
+export interface Price {
+  main: number;
+  fractional: number;
+}
+
+export interface Product {
+  id: number;
+  name: string;
+  price: Price;
+}
+
+export interface CartItem {
+  product: Product;
+  quantity: number;
+}
